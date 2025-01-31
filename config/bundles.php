0f35cde03a6eb91ec0095665c26b7ad825f45c18
aritz <aritz@softspring.es>
2025-01-31 13:19:25 +0100

    Add text

--- config/bundles.php
+++ config/bundles.php
@@ -23,4 +23,5 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Softspring\Symfonic\SymfonicBundle::class => ['all' => true],
     Softspring\TranslatableBundle\SfsTranslatableBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
