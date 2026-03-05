4b1411adfa5f82488ee64e2a17f9dd288cb1402c
Javi H. Gil <javihgil@softspring.es>
2026-03-05 15:09:14 +0100

    Remove polymorphic-form-type deprecated file

--- assets/admin.js
+++ assets/admin.js
@@ -9,5 +9,5 @@ window.bootstrap = bootstrap;
 import '@softspring/cms-bundle/scripts/admin-cms';
 import '@softspring/media-bundle/scripts/media-type';
 import '@softspring/cms-module-collection/modules';
-import '@softspring/polymorphic-form-type/scripts/polymorphic-form-type';
+import '@softspring/collection-form-type/scripts/collection-form-type';
 
